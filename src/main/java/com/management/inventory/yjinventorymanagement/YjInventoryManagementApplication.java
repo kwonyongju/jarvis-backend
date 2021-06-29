abc897794f2ec4d426bb4eb0b1c2d7f28a51f674
yongjukwon <michaelkyj@gmail.com>
2021-06-28 19:03:40 -0700

    Initialize project/ package Spring Boot with React

---
+++ src/main/java/com/management/inventory/yjinventorymanagement/YjInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.management.inventory.yjinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/inventory/yjinventorymanagement/YjInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.inventory.yjinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
