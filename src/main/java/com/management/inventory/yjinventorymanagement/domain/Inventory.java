39f344b981d16f031838efefe5b08458d728ebbf
yongjukwon <michaelkyj@gmail.com>
2021-06-29 13:45:23 -0700

    Add Inventory domain specification

--- src/main/java/com/management/inventory/yjinventorymanagement/domain/Inventory.java
+++ src/main/java/com/management/inventory/yjinventorymanagement/domain/Inventory.java
@@ -1,13 +1,19 @@
 package com.management.inventory.yjinventorymanagement.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import com.management.inventory.yjinventorymanagement.domain.Ingredient.Ingredient;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table
 public class Inventory {
 
     @Id
     @GeneratedValue
     private Long Id;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "inventory")
+    private List<Ingredient> ingredients = new ArrayList<>();
 }
