4ddc03f9c620983011b6f28707731c714d4fb48e
yongjukwon <michaelkyj@gmail.com>
2021-07-01 14:59:27 -0700

    Add addIngredient service

--- src/main/java/com/management/inventory/yjinventorymanagement/service/IngredientService.java
+++ src/main/java/com/management/inventory/yjinventorymanagement/service/IngredientService.java
@@ -1,5 +1,6 @@
 package com.management.inventory.yjinventorymanagement.service;
 
+import com.management.inventory.yjinventorymanagement.domain.Ingredient.Ingredient;
 import com.management.inventory.yjinventorymanagement.repository.IngredientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,10 @@ import org.springframework.stereotype.Service;
 public class IngredientService {
 
     private final IngredientRepository ingredientRepository;
+
+    public Long addIngredient(Ingredient ingredient) {
+        Ingredient ingredientSaved = ingredientRepository.save(ingredient);
+
+        return ingredientSaved.getId();
+    }
 }
