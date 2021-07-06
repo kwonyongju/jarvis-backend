a26a36925f94aa1cc129c9c8b3797d4683dfb370
yongjukwon <michaelkyj@gmail.com>
2021-07-06 14:48:06 -0700

    Add general rest api response wrapper class

--- src/main/java/com/management/inventory/yjinventorymanagement/api/GetResponse.java
+++ src/main/java/com/management/inventory/yjinventorymanagement/api/GetResponse.java
@@ -1,2 +1,11 @@
-package com.management.inventory.yjinventorymanagement.api;public class GetResponse {
+package com.management.inventory.yjinventorymanagement.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GetResponse<T> {
+    private int count;
+    private T data;
 }
