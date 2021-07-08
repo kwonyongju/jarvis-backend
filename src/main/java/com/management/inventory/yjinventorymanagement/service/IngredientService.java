a68b3723501007bd1bf79c89fd55e9ec4efa7562
yongjukwon <michaelkyj@gmail.com>
2021-07-08 09:10:19 -0700

    Add a method to store ingredient w/ its name

--- src/main/java/com/management/inventory/yjinventorymanagement/service/IngredientService.java
+++ src/main/java/com/management/inventory/yjinventorymanagement/service/IngredientService.java
@@ -1,6 +1,7 @@
 package com.management.inventory.yjinventorymanagement.service;
 
 import com.management.inventory.yjinventorymanagement.domain.Ingredient.Ingredient;
+import com.management.inventory.yjinventorymanagement.domain.Ingredient.IngredientFactory;
 import com.management.inventory.yjinventorymanagement.repository.IngredientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,18 @@ import java.util.List;
 public class IngredientService {
 
     private final IngredientRepository ingredientRepository;
+    private final InventoryService inventoryService;
 
     public Long add(Ingredient ingredient) {
         Ingredient ingredientSaved = ingredientRepository.save(ingredient);
+
+        return ingredientSaved.getId();
+    }
+
+    public Long add(String ingredientName) {
+        Ingredient ingredient = IngredientFactory.createIngredient(ingredientName);
+        Ingredient ingredientSaved = ingredientRepository.save(ingredient);
+        inventoryService.addStock(ingredientName, 0);
 
         return ingredientSaved.getId();
     }
