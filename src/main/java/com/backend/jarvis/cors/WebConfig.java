41aa7de75bdf558c8187c42901a13513878d0445
yongjukwon <michaelkyj@gmail.com>
2021-07-13 21:04:10 -0700

    Update cors configuration

--- src/main/java/com/backend/jarvis/cors/WebConfig.java
+++ src/main/java/com/backend/jarvis/cors/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("https://yongju-kwon.com/")
                 .allowedMethods("GET", "POST");
     }
